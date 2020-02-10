4ae88766d9e43243060fa20af01198784b0705c0
mizhidan <dj9824@126.com>
2020-02-10 19:03:19 +0800

    [danjie] feat: class Student

--- 002_object/src/main/java/Application.java
+++ 002_object/src/main/java/Application.java
@@ -1,6 +1,13 @@
+import model.Student;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        Student stu = new Student();
+        stu.age = 20;
+        stu.name = "李莉";
+        stu.gender = "female";
+        stu.rest();
+        stu.study();
     }
 }
---
+++ 002_object/src/main/java/model/Student.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Student {
+    public String name;
+    public int age;
+    public String gender;
+
+    public void study() {
+        System.out.println("读书\n");
+    }
+
+    public void rest() {
+        System.out.println("睡觉\n");
+    }
+}
